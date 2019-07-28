d62378f9fba003ab2b35f1520282285809a3ab1b
Darren Reid <dreid@callancomputers.ie>
2019-07-28 17:40:18 +0100

    Added comment on StringPattern to explain the reason for 2 regex patterns.

--- src/main/java/enums/StringPattern.java
+++ src/main/java/enums/StringPattern.java
@@ -2,6 +2,10 @@ package enums;
 
 import lombok.Getter;
 
+/**
+ * The reason for 2 regex patterns is if the input comes in as 0:00:000 I want to format that to read 00:00:000 to make it
+ * easier for future operations.
+ */
 @Getter
 public enum StringPattern {
 	INPUT_PATTERN_1("\\[[A-Z][A-Za-z0-9]\\]\\s\\d:\\d{2}.\\d{3}"),
